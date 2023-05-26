e627d4436f501d588e5cea3d01c511122e4c9ed0
Drew Bidmead <dmbidmead@gmail.com>
2023-05-26 17:01:29 -0400

    Display functionality with year discount implemented

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const pageviewsDisplay = document.querySelector('#pageviews-display');
+const priceDisplay = document.querySelector('#price-display');
 
+const stopsPageviewValues = ['10K', '50K', '100K', '500K', '1M'];
+const stopsPriceValues = [8, 12, 16, 24, 36];
+let yearlyDiscount = false;
 
 const toggle = document.querySelector('.toggle');
-
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('toggled');
+    yearlyDiscount = !yearlyDiscount;
+    setDisplays();
 })
 
 
@@ -14,6 +20,7 @@ slider.width = slider.containerXRight - slider.containerX;
 slider.priceSegmentWidth = slider.width / 4;
 slider.fill = document.querySelector('.slider-fill');
 slider.handle = document.querySelector('.slider-handle');
+slider.handle.snapIndex = 2;
 
 slider.handle.addEventListener('mousedown', () => {
     window.addEventListener('mousemove', handleMouseMove)
@@ -21,9 +28,8 @@ slider.handle.addEventListener('mousedown', () => {
         document.querySelectorAll('*').forEach(el => {
             el.style.cursor = ''; // setting el.style to an empty sting in the js file will revert the element style to the CSS you defined in the stylesheet
         });
-        slider.handle.style.left = slider.handle.snapTo + 'px';
-        slider.fill.style.width = 100 * parseFloat(slider.handle.style.left)/parseFloat(getComputedStyle(slider).width) + '%'
-        window.removeEventListener('mousemove', handleMouseMove)
+        window.removeEventListener('mousemove', handleMouseMove);
+        handleSnap();
     })
 })
 
@@ -48,10 +54,25 @@ function handleIdentifyStop() {
         if(leftPos >= i*slider.priceSegmentWidth && leftPos < (i+1)*slider.priceSegmentWidth) {
             if(Math.abs(leftPos - i*slider.priceSegmentWidth) >= (Math.abs(leftPos - (i+1)*slider.priceSegmentWidth))) {
                 // will snap to stops[i+1]
-                slider.handle.snapTo = slider.stops[i+1];
+                slider.handle.snapIndex = i+1;
             } else {
-                slider.handle.snapTo = slider.stops[i];
+                slider.handle.snapIndex = i;
             }
         }
     }
+}
+
+function handleSnap() {
+    slider.handle.snapTo = slider.stops[slider.handle.snapIndex];
+    slider.handle.style.left = slider.handle.snapTo + 'px';
+    slider.fill.style.width = 100 * parseFloat(slider.handle.style.left)/parseFloat(getComputedStyle(slider).width) + '%';
+
+    setDisplays();
+}
+
+function setDisplays() {
+    pageviewsDisplay.textContent = stopsPageviewValues[slider.handle.snapIndex] + ' PAGEVIEWS';
+    priceDisplay.textContent = yearlyDiscount ? 
+        '$' + 0.75 * stopsPriceValues[slider.handle.snapIndex] :
+        '$' + stopsPriceValues[slider.handle.snapIndex];
 }
